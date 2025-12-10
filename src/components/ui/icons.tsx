c574fb859be6b6831bc3af3870fe3c63f72731cc
Jean <srjean21@gmail.com>
2025-12-10 18:53:49 -0400

    feat: se añadieron los íconos del <nav>

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -1,8 +1,14 @@
 import type { IconProps as P } from "solid-icons";
+import type { NavLinkName } from "~/components/header";
 import { TbBrightnessHalf, TbSunHigh, TbMoonStars } from "solid-icons/tb";
 import { FaSolidCircleCheck } from "solid-icons/fa";
 import { AiFillWarning } from "solid-icons/ai";
 import { FaSolidCircleInfo } from "solid-icons/fa";
+import { RiDocumentBook3Line, RiDocumentBook3Fill } from "solid-icons/ri";
+import { RiDocumentFileTextLine, RiDocumentFileTextFill } from "solid-icons/ri";
+import { BiRegularHomeAlt2, BiSolidHomeAlt2 } from "solid-icons/bi";
+import { RiUserFacesGroupLine, RiUserFacesGroupFill } from "solid-icons/ri";
+import type { JSX } from "solid-js";
 
 export default {
   sun: (p?: P) => <TbSunHigh {...p} />,
@@ -12,5 +18,22 @@ export default {
     success: (p?: P) => <FaSolidCircleCheck {...p} />,
     error: (p?: P) => <AiFillWarning {...p} />,
     info: (p?: P) => <FaSolidCircleInfo {...p} />,
+  },
+  nav: {
+    outline: {
+      home: (p?: P) => <BiRegularHomeAlt2 {...p} />,
+      subjects: (p?: P) => <RiDocumentBook3Line {...p} />,
+      exams: (p?: P) => <RiDocumentFileTextLine {...p} />,
+      users: (p?: P) => <RiUserFacesGroupLine {...p} />,
+    },
+    solid: {
+      home: (p?: P) => <BiSolidHomeAlt2 {...p} />,
+      subjects: (p?: P) => <RiDocumentBook3Fill {...p} />,
+      exams: (p?: P) => <RiDocumentFileTextFill {...p} />,
+      users: (p?: P) => <RiUserFacesGroupFill {...p} />,
+    },
+  } as {
+    outline: Record<NavLinkName, (p: P) => JSX.Element>;
+    solid: Record<NavLinkName, (p: P) => JSX.Element>;
   },
 } as const;
