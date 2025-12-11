a0fc6baef267bfb962faeb5efd6d8562c415f13e
Jean <srjean21@gmail.com>
2025-12-10 23:17:42 -0400

    feat: Ahora se puede indicar un label

--- src/components/theme-select.tsx
+++ src/components/theme-select.tsx
@@ -25,40 +25,42 @@ const classes = {
     "flex items-center justify-center rounded-full size-6.5 select-none transition-[background-color,color] peer-disabled:(opacity-50 cursor-not-allowed) peer-not-checked:(text-muted-text) peer-checked:(bg-primary text-primary-text shadow-[--btn-shadow]) peer-not-[:checked,:disabled]:(cursor-pointer hover:(bg-highlight shadow-[--elevated-shadow])) peer-checked:[&>svg>path:nth-child(2)]:fill-current peer-focus-visible:(outline-(1 solid current)))",
 };
 
-export default (props: { class?: string }) => {
+export default (props: { class?: string; label: JSX.Element }) => {
   const { userTheme, setTheme } = useTheme();
 
   return (
     <ClientOnly
       fallback={
         <div
-          role="radiogroup"
           class={props.class ? `${classes.root} ${props.class}` : classes.root}
         >
-          <div role="presentation" class={classes.list}>
-            <For each={themeOptions}>
-              {(option) => (
-                <div class={classes.item}>
-                  <input
-                    id={"theme-" + option.value}
-                    class="sr-only"
-                    type="radio"
-                    name="theme"
-                    disabled
-                    value={option.value}
-                  />
-                  <label
-                    for={"theme-" + option.value}
-                    class={
-                      classes.itemLabel + " !opacity-25 !cursor-not-allowed"
-                    }
-                  >
-                    <span class="sr-only">{option.label}</span>
-                    <option.icon />
-                  </label>
-                </div>
-              )}
-            </For>
+          {props.label}
+          <div role="radiogroup">
+            <div role="presentation" class={classes.list}>
+              <For each={themeOptions}>
+                {(option) => (
+                  <div class={classes.item}>
+                    <input
+                      id={"theme-" + option.value}
+                      class="sr-only"
+                      type="radio"
+                      name="theme"
+                      disabled
+                      value={option.value}
+                    />
+                    <label
+                      for={"theme-" + option.value}
+                      class={
+                        classes.itemLabel + " !opacity-25 !cursor-not-allowed"
+                      }
+                    >
+                      <span class="sr-only">{option.label}</span>
+                      <option.icon />
+                    </label>
+                  </div>
+                )}
+              </For>
+            </div>
           </div>
         </div>
       }
@@ -70,9 +72,13 @@ export default (props: { class?: string }) => {
         // @ts-expect-error: el valor es un tema correcto
         onChange={setTheme}
       >
-        <SegmentedControl.Label class="sr-only">
-          Seleccionar tema del sitio
-        </SegmentedControl.Label>
+        {props.label ? (
+          <SegmentedControl.Label>{props.label}</SegmentedControl.Label>
+        ) : (
+          <SegmentedControl.Label class="sr-only">
+            Seleccionar tema del sitio
+          </SegmentedControl.Label>
+        )}
 
         <div role="presentation" class={classes.list}>
           <For each={themeOptions}>
